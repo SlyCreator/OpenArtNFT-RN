92f40ea09e41a73f398b6f72990da1b02a2cdafe
slycreator <chinonso1amaechi@gmial.com>
2022-05-16 09:25:19 +0100

    refactor header

--- navigation/index.tsx
+++ navigation/index.tsx
@@ -20,6 +20,7 @@ import FirstScreen from '../screens/FirstScreen'
 import tw from 'tailwind-react-native-classnames'
 import StackScreen from '../components/StackScreen'
 import ItemDetailsScreen from '../screens/ItemDetailsScreen'
+import DiscoverCreatorScreen from '../screens/DiscoverCreatorScreen'
 
 
 
@@ -80,115 +81,10 @@ const Header = (props: NativeStackHeaderProps) => {
 export function RootNavigator({ initialRouteName }: RootNavigationProps) {
   return (
     <Stack.Navigator >
+      <Stack.Screen name="discover" component={DiscoverCreatorScreen} options={{headerShown:false}}/>
       <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
       <Stack.Screen name="ItemDetails" component={ItemDetailsScreen} options={{headerShown:false}} />
       <Stack.Screen name="Welcome" component={FirstScreen} options={{ headerShown: false }} />
      </Stack.Navigator>
-  )
-}
-
-const BottomTab = createBottomTabNavigator<RootStackParamList>()
-
-
-function BottomTabNavigator() {
-  const [state, setState] = React.useState<string>()
-  return (
-    <BottomTab.Navigator
-      initialRouteName="Home"
-      screenOptions={{
-        tabBarActiveTintColor: '#4B4780',
-        tabBarShowLabel: false,
-        headerShown: false,
-        tabBarStyle: {
-          backgroundColor: '#F8F7F7',
-          padding: 20,
-        },
-      }}
-    >
-      <BottomTab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={({ route: route1 }) => {
-          return {
-            title: 'Home',
-            headerShown: false,
-            tabBarIcon: ({ color }) => (
-              <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                <SimpleLineIcons name="home" size={24} color={color} />
-                <Text style={{ color, textAlign: 'center' }}>Home</Text>
-              </View>
-            ),
-          }
-        }}
-      />
-
-      <BottomTab.Screen
-        name="Menu"
-        component={MenuScreen}
-        options={({ route: route2 }) => {
-          return {
-            title: 'Menu',
-            headerShown: false,
-            tabBarIcon: ({ color }) => (
-              <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                <Feather name="coffee" size={24} color={color} />
-                <Text style={{ color, textAlign: 'center' }}>Menu</Text>
-              </View>
-            ),
-          }
-        }}
-      />
-
-      <BottomTab.Screen
-        name="Order"
-        component={OrderScreen}
-        options={({ route: route3 }) => {
-          return {
-            title: 'Order',
-            headerShown: false,
-            tabBarIcon: ({ color }) => (
-              <View style={{ justifyContent: 'center', alignItems: 'center' }}>
-                <MaterialIcons name="list-alt" size={24} color={color} />
-                <Text style={{ color, textAlign: 'center' }}>Order</Text>
-              </View>
-            ),
-          }
-        }}
-      />
-
-      <BottomTab.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={({ route: route4 }) => {
-          return {
-            title: 'Home',
-            headerShown: false,
-            tabBarIcon: ({ color }) => (
-              <View style={{ justifyContent: 'center', alignItems: 'center', ...[tw`mb-2`] }}>
-                <MaterialCommunityIcons name="account-circle" size={20} color={color} />
-                <Text style={{ color, textAlign: 'center' }}>Profile</Text>
-              </View>
-            ),
-          }
-        }}
-      />
-
-
-
-    </BottomTab.Navigator>
-  )
-}
-
-
-
-/**
- * You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
- */
-function TabBarIcon(props: {
-  name: React.ComponentProps<typeof MaterialCommunityIcons>['name']
-  color: string
-}) {
-  return (
-    <MaterialCommunityIcons size={30} style={{ marginBottom: -3 }} {...props} />
   )
 }
